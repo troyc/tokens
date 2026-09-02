6943dc6263927a7e2ceaf2725fb5a41b098175da
Troy Crosley <troycrosley@gmail.com>
2026-09-01 21:00:42 -0400

    Revert "completions"
    This reverts commit e4f1c65553a6646a531cd70b0a55f0915722efcd.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::io;
 use std::path::PathBuf;
 
 use anyhow::Result;
-use clap::{CommandFactory, Parser, ValueHint};
-use clap_complete::{Shell, generate};
+use clap::Parser;
 
 use tc::classify::breakdown;
 use tc::count::count;
@@ -21,23 +19,13 @@ struct Cli {
     #[arg(short, long)]
     lockfiles: bool,
 
-    /// Print a completion script for the given shell.
-    #[arg(long, value_enum, value_name = "SHELL")]
-    generate_completion: Option<Shell>,
-
     /// File or directory to count.
-    #[arg(default_value = ".", value_hint = ValueHint::AnyPath)]
+    #[arg(default_value = ".")]
     path: PathBuf,
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    if let Some(shell) = cli.generate_completion {
-        let mut cmd = Cli::command();
-        let name = cmd.get_name().to_string();
-        generate(shell, &mut cmd, name, &mut io::stdout());
-        return Ok(());
-    }
     let files = walk::collect_files(&cli.path, cli.lockfiles)?;
     let mut rows = Vec::new();
     for file in files {
